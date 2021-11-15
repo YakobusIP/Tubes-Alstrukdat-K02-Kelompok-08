26733b5b7c879413982af37def57cb2559437675
pandora-1 <michaelkahari.mk@gmail.com>
2021-11-15 13:14:30 +0700

    Test

--- src/COMMAND/loadFile.c
+++ src/COMMAND/loadFile.c
@@ -1,28 +1,78 @@
 #include <stdio.h>
-#include "ADT Mesin Kata/tokenmachine.h"
-#include "ADT Mesin Kata/tokenmachine.c"
-#include "ADT Mesin Kata/charmachine.h"
-#include "ADT Mesin Kata/charmachine.c"
-#include "ADT Linked List/todoQueue.h"
-#include "ADT Linked List/todoQueue.c"
-Token currentToken;
-int main() {
+#include "loadFile.h"
+
+void readConfigFile(PrioQueue *q, map *M, adjMatrix *A) {
+/* Membaca file config secara penuh */
     /* KAMUS */
-    toDoList val;
-    PrioQueue q;
+    requestList val;
+    static FILE *saveFile;
     int i, p;
     int inreqIn, intimeLimit;
     char inpickUp, indropOff, intype;
     /* ALGORITMA */
-    // START READ SAVE FILE
-    start(saveFileName);
+    // START READ CONFIG FILE
+    printf("Masukkan nama save file dalam permainan : ");
+    readCommand();
+    saveFile = fopen(currentToken.contents,"a");
+
+    start(configFileName);
     ignoreBlank();
     ignoreNext();
+   
+    //READ MAP SIZE
+    int mapRow, mapCol, hqRow, hqCol, coordLength, coordRow, coordCol, adj;
+    char coordName;
+    ignoreBlank();
+    mapRow = readNumberfromChar();
+    ignoreBlank();
+    mapCol = readNumberfromChar();
+    ignoreNext();
+    CreateMap(M, mapRow, mapCol);
 
+    //KOORDINAT HQ
+    ignoreBlank();
+    hqRow = readNumberfromChar();
+    ignoreBlank();
+    hqCol = readNumberfromChar();
+    ignoreNext();
+    Coordinate *C = CreateCoord('8', hqRow, hqCol);
+    ReadMap(M, C);
+
+
+   
+    //CEK KOORDINAT
+    ignoreBlank();
+    coordLength = readNumberfromChar();
+    for(int j = 0; j < coordLength; j++)
+    {
+        ignoreNext();
+        ignoreBlank();
+        coordName = currentChar;
+        adv();
+        coordRow = readNumberfromChar();
+        coordCol = readNumberfromChar();
+        Coordinate *C = CreateCoord(coordName, coordRow, coordCol);
+        ReadMap(M, C);
+    }
+
+    //READ ADJ MATRIX
+    createAdjMatrix(A, coordLength + 1);
+
+    for(int j = 0; j < coordLength + 1; j++)
+    {
+        ignoreNext();
+        ignoreBlank();
+        for(int k = 0; k < coordLength + 1; k++)
+        {
+            ignoreBlank();
+            adj = readNumberfromChar();
+            ReadAdjMatrix(A, j, k, adj);
+        }
+    }
     // READ DAFTAR PESANAN DAN INPUT KE TO DO LIST
     p = readNumberfromChar();
-    
-    CreatePrioQueue(&q);
+    //printf("%d,", p);
+    CreatePrioQueue(q);
     for (i=0;i<p;i++) {
         ignoreNext();
         ignoreBlank();
@@ -58,7 +108,39 @@ int main() {
         val.timeLimit = intimeLimit;
 
         // INPUT VAL YANG SUDAH DIBENTUK KE DALAM QUEUE
-        enqueueToDo(&q, val, 10);
+        enqueueRL(q, val);
     }
-    return 0;
+}
+
+void saveToFile(PrioQueue *q) {
+/* Save state game ke dalam sebuah file */   
+    /* KAMUS */
+    requestList val;
+    int i, len;
+    /* ALGORITMA */
+    saveFile = fopen(saveFileName,"w");
+
+    // Print banyaknya pesanan
+    fprintf(saveFile, "%d\n", length(*q));
+
+    // Print everything inside Daftar Pesanan or To Do List
+    len = length(*q);
+    for (i=0;i<len;i++) {
+        dequeue(q, &val);
+        fprintf(saveFile, "%d ", val.reqIn);
+        fprintf(saveFile, "%c ", val.pickUp);
+        fprintf(saveFile, "%c ", val.dropOff);
+        fprintf(saveFile, "%c", val.type);
+
+        if (val.type == 'P') {
+            fprintf(saveFile, " %d\n", val.timeLimit);
+        } else {
+            fprintf(saveFile, "\n");
+        }
+        if (i==len) {
+            fprintf(saveFile, "");
+        }
+    }
+
+    fclose(saveFile);
 }
---
+++ src/COMMAND/loadFile.h
@@ -0,0 +1,21 @@
+#ifndef LOAD_H
+#define LOAD_H
+
+#include "boolean.h"
+#include "../ADT Lain/UangWaktu.h"
+#include "../ADT Linked List/todoNode.h" 
+#include "../ADT List/adjList.h"
+#include "../ADT List/Gadget.h"
+#include "../ADT List/InventoryGadget.h"
+#include "../ADT Matriks/adjMatriks.h"
+#include "../ADT Matriks/map.h"
+#include "../ADT Mesin Kata/charmachine.h"
+#include "../ADT Mesin Kata/tokenmachine.h"
+#include "../ADT Point/point.h"
+#include "../ADT Stack/stack.h"
+#include "../ADT Queue/requestList.h"
+#include "../pcolor/pcolor.h"
+
+void load(PrioQueue *q, map *M, adjMatrix *A);
+
+#endif
